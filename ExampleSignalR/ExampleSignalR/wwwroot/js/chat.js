c0e869ec3f517fbbb99336e8e8b1808638b6e960
BIZVOX\eduardo.prudencio <eduardo.lobo@bemovin.com.br>
2019-03-12 15:43:44 -0300

    Primeira versão de um exemplo de SignalR

---
+++ ExampleSignalR/ExampleSignalR/wwwroot/js/chat.js
@@ -0,0 +1,61 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg        = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says... " + msg;
+    var li         = document.createElement("li");
+    li.textContent = encodedMsg;
+
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+//document.getElementById("sendButton").addEventListener("click", function (event) {
+//    var user    = document.getElementById("userInput").value;
+//    //var message = document.getElementById("messageInput").value;
+//    var message = new Date().toLocaleString();
+
+//    connection.invoke("SendMessage", user, message).catch(function (err) {
+//        return console.error(err.toString());
+//    });
+
+//    event.preventDefault();
+//});
+
+
+//var millisecondsToWait = 500;
+
+//setTimeout(function () {
+
+//    var user = document.getElementById("userInput").value;
+//    var message = new Date().toLocaleString();
+
+//    connection.invoke("SendMessage", user, message).catch(function (err) {
+//        return console.error(err.toString());
+//    });
+
+//}, millisecondsToWait);
+
+//var cont = 0;
+
+//while (cont < 10)
+{
+    var user = document.getElementById("userInput").value;
+    var message = new Date().toLocaleString();
+
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+  //  cont++;
+}
